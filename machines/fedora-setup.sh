083d2d3384d120eb66205f42f99156140f849573
Jake Petroules <jake.petroules@petroules.com>
2014-01-27 09:14:31 -0500

    Set up Fedora the same way as before.

--- machines/fedora-setup.sh
+++ machines/fedora-setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 
-sudo yum -y install git gcc gcc-c++ qt-devel cmake rpm-build doxygen pandoc
+if [ "$(id -u)" != "0" ] ; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+yum groupinstall 'Development Tools'
+yum install git rpm rpm-build qt qt-devel cmake SDL-devel gtk2-devel doxygen pandoc
