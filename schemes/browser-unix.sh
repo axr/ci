5a7ac8fbd26852f4acc9870b9eff0dd7ccfeecb0
Jake Petroules <jake.petroules@petroules.com>
2014-01-27 09:27:32 -0500

    Add browser scheme.

---
+++ schemes/browser-unix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git clone --recursive https://github.com/axr/common.git
+git clone --recursive https://github.com/axr/core.git
+git clone --recursive https://github.com/axr/framework.git
+mkdir _build
+cd _build
+cmake -DCMAKE_BUILD_TYPE=Release ../browser
+make all distribution package_source
