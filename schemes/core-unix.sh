c8c2c3a324465eb0cf78d264e783f4b55f2bd249
Jake Petroules <jake.petroules@petroules.com>
2014-01-27 09:23:47 -0500

    BUILD_SHARED_LIBS is default; don't need it.

--- schemes/core-unix.sh
+++ schemes/core-unix.sh
@@ -5,5 +5,5 @@ git clone --recursive https://github.com/axr/common.git
 git clone --recursive https://github.com/axr/core.git
 mkdir _build
 cd _build
-cmake -DCMAKE_BUILD_TYPE=Release -DAXR_BUILD_CORE=ON -DAXR_BUILD_DOCS=ON -DBUILD_SHARED_LIBS=ON ../core
+cmake -DCMAKE_BUILD_TYPE=Release -DAXR_BUILD_CORE=ON -DAXR_BUILD_DOCS=ON ../core
 make all distribution package_source
